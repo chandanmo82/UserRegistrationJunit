87a9e1e755e3b7cd7db7ed4c92835f271c9a77c4
Chandan <chandanmohanty11111>
2021-10-19 20:27:29 +0530

    ADDED:Testcases To Check All the EmailAddress

--- src/main/java/com/bridgelabz/userregistrationjunit/UserRegistration.java
+++ src/main/java/com/bridgelabz/userregistrationjunit/UserRegistration.java
@@ -4,10 +4,10 @@ public class UserRegistration {
     static String NAME_PATTERN = "^[A-Z][a-z]{2,}$";
     static String EMAIL_PATTERN = "^([a-zA-Z]{3,}([.|_|+|-]?[a-zA-Z0-9]+)?[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.]?[a-zA-Z]{2,3})?)$";
     static String MOBILE_NUMBER_PATTERN = "^[0-9]{1,3}[' '][0-9]{10}$";
-    static String PASSWORD_AT_LEAST_EIGHT_CHARACTER_LONG="[a-z]{8,}";
-    static String PASSWORD_AT_LEAST_ONE_UPPERCASE_CHARACTER="(?=.*[a-z])(?=.*[A-Z]).{8,40}";
-    static String PASSWORD_AT_LEAST_ONE_Number="((?=.*[a-z])(?=.*[0-9])(?=.*[A-Z]).{8,40})";
-    static String PASSWORD_HAS_EXACTLY_ONE_SYMBOL="^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=[^$@!#%*?&]*[$#@!%*?&][^$@!#%*?&]*$).{8,}";
+    static String PASSWORD_AT_LEAST_EIGHT_CHARACTER_LONG = "[a-z]{8,}";
+    static String PASSWORD_AT_LEAST_ONE_UPPERCASE_CHARACTER = "(?=.*[a-z])(?=.*[A-Z]).{8,40}";
+    static String PASSWORD_AT_LEAST_ONE_Number = "((?=.*[a-z])(?=.*[0-9])(?=.*[A-Z]).{8,40})";
+    static String PASSWORD_HAS_EXACTLY_ONE_SYMBOL = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=[^$@!#%*?&]*[$#@!%*?&][^$@!#%*?&]*$).{8,}";
 
     public static boolean validInputName(String inputName) {
         return inputName.matches(NAME_PATTERN);
@@ -20,11 +20,12 @@ public class UserRegistration {
     public static boolean validateMobileNumber(String mobileNumber) {
         return mobileNumber.matches(MOBILE_NUMBER_PATTERN);
     }
-    public static boolean validateEightCharacterLongPassword(String passwordAtLeastEightCharacterLong ) {
+
+    public static boolean validateEightCharacterLongPassword(String passwordAtLeastEightCharacterLong) {
         return passwordAtLeastEightCharacterLong.matches(PASSWORD_AT_LEAST_EIGHT_CHARACTER_LONG);
     }
 
-    public static boolean validatePassword(String passWord , String pattern) {
+    public static boolean validatePassword(String passWord, String pattern) {
         return passWord.matches(pattern);
     }
 
