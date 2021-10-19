20c25c956ec75354deacd1f82f5832dd95049e60
Chandan <chandanmohanty11111>
2021-10-19 19:36:17 +0530

    ADDED:Testcase and Method For Validate MobileNumber

--- src/main/java/com/bridgelabz/userregistrationjunit/UserRegistration.java
+++ src/main/java/com/bridgelabz/userregistrationjunit/UserRegistration.java
@@ -2,7 +2,9 @@ package com.bridgelabz.userregistrationjunit;
 
 public class UserRegistration {
     static String NAME_PATTERN = "^[A-Z][a-z]{2,}$";
-    static  String EMAIL_PATTERN = "^([a-zA-Z]{3,}([.|_|+|-]?[a-zA-Z0-9]+)?[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.]?[a-zA-Z]{2,3})?)$";
+    static String EMAIL_PATTERN = "^([a-zA-Z]{3,}([.|_|+|-]?[a-zA-Z0-9]+)?[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.]?[a-zA-Z]{2,3})?)$";
+    static String MOBILE_NUMBER_PATTERN = "^[0-9]{1,3}[' '][0-9]{10}$";
+
 
     public static boolean validInputName(String inputName) {
         return inputName.matches(NAME_PATTERN);
@@ -10,6 +12,10 @@ public class UserRegistration {
 
     public static boolean validateEmailId(String emailId) {
         return emailId.matches(EMAIL_PATTERN);
+    }
+
+    public static boolean validateMobileNumber(String mobileNumber) {
+        return mobileNumber.matches(MOBILE_NUMBER_PATTERN);
     }
 
     public static void main(String[] args) {
--- src/test/java/com/bridgelabz/userregistrationjunit/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/userregistrationjunit/UserRegistrationTest.java
@@ -9,25 +9,21 @@ public class UserRegistrationTest {
         boolean firstName = UserRegistration.validInputName("Sameer");
         Assert.assertTrue(firstName);
     }
-
     @Test
     public void givenFirstName_WhenImproper_ShouldReturnFalse() {
         boolean firstName =UserRegistration.validInputName("ameer13");
         Assert.assertFalse(firstName);
     }
-
     @Test
     public void givenLastName_WhenProper_ShouldReturnTrue(){
         boolean lastName = UserRegistration.validInputName("Patil");
         Assert.assertTrue(lastName);
     }
-
     @Test
     public void givenLastName_WhenImproper_ShouldReturnFalse() {
         boolean lastName =UserRegistration.validInputName("PAtil7");
         Assert.assertFalse(lastName);
     }
-
     @Test
     public void givenEmailId_WhenProper_ShouldReturnFalse() {
         boolean emailId = UserRegistration.validateEmailId("abc@gail.com");
@@ -37,5 +33,15 @@ public class UserRegistrationTest {
     public void givenEmailId_WhenImproper_ShouldReturnFalse() {
         boolean emailId = UserRegistration.validateEmailId("abcgail.com");
         Assert.assertFalse(emailId);
+    }
+    @Test
+    public void givenMobileNumber_WhenProper_ShouldReturnTrue() {
+        boolean mobileNumber = UserRegistration.validateMobileNumber("91 8605863744");
+        Assert.assertTrue(mobileNumber);
+    }
+    @Test
+    public void givenMobileNumber_WhenImproper_ShouldReturnFalse() {
+        boolean mobileNumber = UserRegistration.validateMobileNumber("91 18605863744");
+        Assert.assertFalse(mobileNumber);
     }
 }
