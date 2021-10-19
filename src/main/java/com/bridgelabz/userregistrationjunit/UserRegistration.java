192dff7b76505376fa80fb250b2d00d15128bbf5
Chandan <chandanmohanty11111>
2021-10-19 18:51:13 +0530

    ADDED:Test Case For Validate FirstName

---
+++ src/main/java/com/bridgelabz/userregistrationjunit/UserRegistration.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.userregistrationjunit;
+
+public class UserRegistration {
+    static String NAME_PATTERN = "^[A-Z][a-z]{2,}$";
+
+    public static boolean validInputName(String inputName) {
+        return inputName.matches(NAME_PATTERN);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to User Registration");
+    }
+
+}
+
